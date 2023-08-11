38a15519587f65b6b5a91fc6b48e0d1637883c01
Kwe <Kweayon@gmail.com>
2023-08-11 16:57:57 -0500

    updated:  updated base scaffold

--- lib/features/shared/ui/base_scaffold.dart
+++ lib/features/shared/ui/base_scaffold.dart
@@ -4,7 +4,12 @@ class BasseScaffold extends StatelessWidget {
   final String title;
   final Widget child;
   final bool showAppBar;
-  const BasseScaffold({required this.title, required this.child, this.showAppBar = false, super.key});
+  const BasseScaffold({
+    required this.title,
+    required this.child,
+    this.showAppBar = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) => SafeArea(
