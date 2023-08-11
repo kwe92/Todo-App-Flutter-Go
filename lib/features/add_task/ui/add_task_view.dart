3d703eadc6c6e5cdca70fe6416b1d8cc37900406
Kwe <Kweayon@gmail.com>
2023-08-11 18:22:38 -0500

    upfate: initial ui for add task view

---
+++ lib/features/add_task/ui/add_task_view.dart
@@ -0,0 +1,18 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_golang_yt/features/shared/ui/base_scaffold.dart';
+
+@RoutePage()
+class AddTaskView extends StatelessWidget {
+  const AddTaskView({super.key});
+
+  Widget build(BuildContext context) => const BasseScaffold(
+        showAppBar: true,
+        title: 'Add Task View',
+        child: SizedBox(
+          child: Column(
+            children: [],
+          ),
+        ),
+      );
+}
