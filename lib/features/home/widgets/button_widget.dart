d7b49edd126fc586c574259a7e1910d3bba4b438
Kwe <Kweayon@gmail.com>
2023-08-11 17:20:54 -0500

    update: added comments | expanded textstyle

--- lib/features/home/widgets/button_widget.dart
+++ lib/features/home/widgets/button_widget.dart
@@ -24,6 +24,7 @@ class CustomButton extends StatelessWidget {
             text,
             style: TextStyle(
               color: textColor,
+              fontSize: 20,
             ),
           ),
           style: ButtonStyle(
@@ -39,10 +40,16 @@ class CustomButton extends StatelessWidget {
 }
 // ?----------------------What did you learn----------------------?
 
-// Container / Sizedbox setting width or height property: double.maxFinite argument
+// Expanding Widgets With Container or SizedBox
 
-//   - passing the argument double.maxFinite to height or width will constrain
-//     the containers child to fill all available space in that direction
-//   - seems like a more granular way to use an Expanded widget
+//   - passing the argument double.maxFinite to height or width will expand
+//     the containers child to fill all available space
+//   - like a granular way to use an Expanded widget
+//   - can be used instead of expanding multiple widgets
 
+// Dynamic styling: MediaQuery.of(context)
+
+//   - you can style dynamically using the MediaQuery.of(context).size height or width
+
+/// utility function that makes calls to MaterialStateProperty.resolveWith shorter.
 MaterialStateProperty<T?>? resolver<T>(T? Function(Set<MaterialState> states) callback) => MaterialStateProperty.resolveWith(callback);
