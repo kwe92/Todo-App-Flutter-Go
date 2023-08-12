423f8bd53d904773ca00f3daa619fbca07e41b88
Kwe <Kweayon@gmail.com>
2023-08-11 23:01:24 -0500

    updare:instantiated text form fields

--- lib/features/add_task/ui/add_task_view.dart
+++ lib/features/add_task/ui/add_task_view.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_golang_yt/features/add_task/ui/add_task_view_model.dart';
+import 'package:flutter_golang_yt/features/add_task/ui/widgets/add_task_text_form_field.dart';
 import 'package:flutter_golang_yt/features/shared/ui/base_scaffold.dart';
+import 'package:provider/provider.dart';
 
 // TODO: finish completing view
 
@@ -8,13 +11,47 @@ import 'package:flutter_golang_yt/features/shared/ui/base_scaffold.dart';
 class AddTaskView extends StatelessWidget {
   const AddTaskView({super.key});
 
-  Widget build(BuildContext context) => const BasseScaffold(
-        showAppBar: true,
-        title: 'Add Task View',
-        child: SizedBox(
+  @override
+  Widget build(BuildContext context) {
+    final taskNameController = context.watch<AddTaskViewModel>().taskNameController;
+    final taskDetailController = context.watch<AddTaskViewModel>().taskDetailController;
+
+    return BasseScaffold(
+      title: 'Add Task View',
+      child: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: _backgroundImage,
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height / 3,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
-            children: [],
+            children: [
+              AddTaskTextForField(
+                controller: taskNameController,
+                rounding: Radius.circular(MediaQuery.of(context).size.width / 2),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              AddTaskTextForField(
+                maxLines: 3,
+                controller: taskDetailController,
+                rounding: const Radius.circular(20),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
+
+const _backgroundImage = BoxDecoration(
+  image: DecorationImage(
+    fit: BoxFit.cover,
+    image: AssetImage('assets/images/addtask1.jpg'),
+  ),
+);
