cdcd13f9ca71a04e191bf0d63d548247c9573120
Kwe <Kweayon@gmail.com>
2023-08-11 23:02:46 -0500

    update: get it services

---
+++ lib/features/shared/services/services.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_golang_yt/app/router/app_router.dart';
+import 'package:flutter_golang_yt/features/shared/services/get_it.dart';
+
+AppRouter get appRouter => locator.get<AppRouter>();
