6e504750c8ee0124fd51a91d62da074674ba98d5
Kwe <Kweayon@gmail.com>
2023-08-12 15:05:33 -0500

    update: added a back button to pop the route off of the stack and some additional todo comments

--- lib/features/add_task/ui/add_task_view.dart
+++ lib/features/add_task/ui/add_task_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/features/add_task/ui/add_task_view_model.dart';
 import 'package:flutter_golang_yt/features/add_task/ui/widgets/add_task_text_form_field.dart';
 import 'package:flutter_golang_yt/features/home/widgets/button_widget.dart';
+import 'package:flutter_golang_yt/features/shared/services/services.dart';
 import 'package:flutter_golang_yt/features/shared/ui/base_scaffold.dart';
 import 'package:provider/provider.dart';
 
@@ -18,43 +19,61 @@ class AddTaskView extends StatelessWidget {
     final taskDetailController = context.watch<AddTaskViewModel>().taskDetailController;
 
     return BasseScaffold(
-      showAppBar: true,
+      // showAppBar: true,
       title: 'Add Task View',
-      child: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: _backgroundImage,
-        padding: EdgeInsets.only(
-          top: MediaQuery.of(context).size.height / 3.5,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              AddTaskTextForField(
-                controller: taskNameController,
-                rounding: Radius.circular(MediaQuery.of(context).size.width / 2),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              // TODO: I dont like how AddTaskTextForField is implemented
-              AddTaskTextForField(
-                maxLines: 3,
-                controller: taskDetailController,
-                rounding: const Radius.circular(20),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              CustomButton(
-                text: 'Add',
-                onPressed: () {},
-              ),
-            ],
+      child: Stack(children: [
+        Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          // TODO: figure out if we can make the image longer or not
+          decoration: _backgroundImage,
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height / 3.5,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              // TODO: I dont like the spacing it looks off
+              children: [
+                AddTaskTextForField(
+                  controller: taskNameController,
+                  rounding: Radius.circular(MediaQuery.of(context).size.width / 2),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                // TODO: I dont like how AddTaskTextForField is implemented
+                AddTaskTextForField(
+                  maxLines: 3,
+                  controller: taskDetailController,
+                  rounding: const Radius.circular(20),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                CustomButton(
+                  text: 'Add',
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+        Container(
+          // TODO: keep playing with the height
+          height: MediaQuery.of(context).size.height / 12,
+          // TODO: remove color when you are done ans turn into a SizedBox
+          // color: Colors.orange,
+          child: GestureDetector(
+            onTap: () => appRouter.pop(),
+            child: const Icon(
+              // TODO: dynamically size this widget
+              size: 50,
+              Icons.arrow_left,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
