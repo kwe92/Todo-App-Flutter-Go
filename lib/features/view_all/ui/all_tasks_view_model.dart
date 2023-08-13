4599dd5f2bc8f189cba2606ed1733f625544f9e4
Kwe <Kweayon@gmail.com>
2023-08-13 17:52:16 -0500

    update: inital all tasks viw model

---
+++ lib/features/view_all/ui/all_tasks_view_model.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+
+typedef Task = ({
+  int id,
+  String taskName,
+  String taskDetails,
+});
+
+class AllTasksViewModel extends ChangeNotifier {
+  List<Task> _tasks = [
+    (
+      id: 1101,
+      taskName: 'Just Keep Coding',
+      taskDetails: 'Clean code is the best code!',
+    ),
+    (
+      id: 1102,
+      taskName: 'Code for 3 hours',
+      taskDetails: 'when attention meets intention.',
+    ),
+    (
+      id: 1103,
+      taskName: 'Begin',
+      taskDetails: 'To begin is half of the work let half still remain, again begin this and though wilt have finish.',
+    ),
+  ];
+
+  List<String> get taskNames => tasks.map((task) => task.taskName as String).toList();
+
+  List get tasks => _tasks;
+
+  void addTask() {}
+  void removeTask() {}
+}
