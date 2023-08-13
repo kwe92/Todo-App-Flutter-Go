1f508fc956722d49a84f09ec12210bd2e8971471
Kwe <Kweayon@gmail.com>
2023-08-13 17:51:28 -0500

    update: implemented add icon

---
+++ lib/features/view_all/ui/widgets/add_icon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_golang_yt/app/colors/app_colors.dart';
+import 'package:flutter_golang_yt/features/shared/ui/clickable_icon.dart';
+
+class AddIcon extends StatelessWidget {
+  const AddIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const double sides = 25;
+    return Container(
+      alignment: Alignment.center,
+      width: sides,
+      height: sides,
+      decoration: const BoxDecoration(
+        color: AppColors.mainColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(sides / 2),
+        ),
+      ),
+      child: ClickableIcon(
+        onTap: () {},
+        iconData: Icons.add,
+        size: 16,
+        color: Colors.white,
+      ),
+    );
+  }
+}
