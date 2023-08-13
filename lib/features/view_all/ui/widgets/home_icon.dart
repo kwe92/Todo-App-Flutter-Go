035134bf134284beab64e95940910a8b02852215
Kwe <Kweayon@gmail.com>
2023-08-13 17:51:48 -0500

    update: implemented home icon

---
+++ lib/features/view_all/ui/widgets/home_icon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_golang_yt/app/router/app_router.gr.dart';
+import 'package:flutter_golang_yt/features/shared/services/services.dart';
+import 'package:flutter_golang_yt/features/shared/ui/clickable_icon.dart';
+
+class HomeIcon extends StatelessWidget {
+  const HomeIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClickableIcon(
+      onTap: () => appRouter.popAndPush(const HomeRoute()),
+      iconData: Icons.home,
+    );
+  }
+}
