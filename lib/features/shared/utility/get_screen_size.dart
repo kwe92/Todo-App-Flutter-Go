5b8d160d3cc5eef3b6e7837e813bcd0e23eb03d4
Kwe <Kweayon@gmail.com>
2023-08-14 01:33:48 -0500

    update: added descriptive comments

--- lib/features/shared/utility/get_screen_size.dart
+++ lib/features/shared/utility/get_screen_size.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+/// utility function that shortens the call to MediaQuery.of(context).size for height and width.
 class ScreenSize {
   const ScreenSize._();
 
+  /// utility function that shortens the call to MediaQuery.of(context).size.height.
   static double getHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
+  /// utility function that shortens the call to MediaQuery.of(context).size.width.
   static double getWidth(BuildContext context) => MediaQuery.of(context).size.width;
 }
