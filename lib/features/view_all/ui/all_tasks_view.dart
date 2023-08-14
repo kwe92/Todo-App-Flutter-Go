1ec3d5b3b09255b836bce3c8ebc4f60bdf5bdc4d
Kwe <Kweayon@gmail.com>
2023-08-14 00:30:13 -0500

    implemented task widget

--- lib/features/view_all/ui/all_tasks_view.dart
+++ lib/features/view_all/ui/all_tasks_view.dart
@@ -1,10 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_golang_yt/app/colors/app_colors.dart';
+import 'package:flutter_golang_yt/declarations.dart';
 import 'package:flutter_golang_yt/features/shared/services/services.dart';
 import 'package:flutter_golang_yt/features/shared/ui/back_arrow_icon.dart';
 import 'package:flutter_golang_yt/features/shared/ui/base_scaffold.dart';
+import 'package:flutter_golang_yt/features/shared/utility/get_screen_size.dart';
+import 'package:flutter_golang_yt/features/view_all/ui/all_tasks_view_model.dart';
 import 'package:flutter_golang_yt/features/view_all/ui/widgets/add_icon.dart';
 import 'package:flutter_golang_yt/features/view_all/ui/widgets/home_icon.dart';
+import 'package:provider/provider.dart';
 
 @RoutePage()
 class AllTasksView extends StatelessWidget {
@@ -12,12 +17,30 @@ class AllTasksView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BasseScaffold(
+    final List<_TaskWidget> taskList = context
+        .read<AllTasksViewModel>()
+        .tasks
+        .map(
+          (task) => _TaskWidget(task: task),
+        )
+        .toList();
+
+    return BasseScaffold(
       title: 'All Tasks View',
       child: Column(
         children: [
-          _TopSecton(),
-          _MiddleSection(),
+          const _TopSecton(),
+          const _MiddleSection(),
+
+          // Nesting ListView in Column Widget
+
+          //   - if you need to next a ListView within a Column widget you need to wrap the ListView with a Flexible Widget
+
+          Flexible(
+            child: ListView(
+              children: taskList,
+            ),
+          )
         ],
       ),
     );
@@ -31,7 +54,7 @@ class _TopSecton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.maxFinite,
-      height: MediaQuery.of(context).size.height / 3.5,
+      height: ScreenSize.getHeight(context) / 3.5,
       alignment: Alignment.topLeft,
       padding: const EdgeInsets.only(
         top: 18,
@@ -70,6 +93,34 @@ class _MiddleSection extends StatelessWidget {
               Icons.file_copy,
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TaskWidget extends StatelessWidget {
+  final Task task;
+  const _TaskWidget({
+    required this.task,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 12.0),
+      width: double.maxFinite,
+      height: ScreenSize.getHeight(context) / 14,
+      color: AppColors.textInputGrey,
+      child: Center(
+        child: Text(
+          task.taskName,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.blueGrey,
+            fontSize: 20,
+          ),
         ),
       ),
     );
