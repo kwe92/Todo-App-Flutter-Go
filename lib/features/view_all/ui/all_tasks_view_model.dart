ba80ac4801979220361226d952739d9b6100553f
Kwe <Kweayon@gmail.com>
2023-08-14 02:40:43 -0500

    update: stated add task implementation

--- lib/features/view_all/ui/all_tasks_view_model.dart
+++ lib/features/view_all/ui/all_tasks_view_model.dart
@@ -4,28 +4,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/declarations.dart';
 
 class AllTasksViewModel extends ChangeNotifier {
-  List<Task> _tasks = [
-    (
+  Map<int, Task> _tasks = {
+    1101: (
       id: 1101,
       taskName: 'Just Keep Coding',
       taskDetails: 'Clean code is the best code!',
     ),
-    (
+    1102: (
       id: 1102,
       taskName: 'Code for 3 hours',
       taskDetails: 'when attention meets intention.',
     ),
-    (
+    1103: (
       id: 1103,
       taskName: 'Begin',
       taskDetails: 'To begin is half of the work let half still remain, again begin this and though wilt have finish.',
     ),
-  ];
+  };
+  // List<String> get taskNames => tasks.map((task) => task.taskName as String).toList();
 
-  List<String> get taskNames => tasks.map((task) => task.taskName as String).toList();
+  List<Task> get taskList => _tasks.values.toList();
 
-  List get tasks => _tasks;
+  void addTask(String taskName, String taskDetails) {
+    // TODO modify implementation when you use real data
+    final taskId = taskList.last.id + 1;
+    _tasks.addAll({
+      taskId: (
+        id: taskId,
+        taskName: taskName,
+        taskDetails: taskDetails,
+      )
+    });
+    notifyListeners();
+  }
 
-  void addTask() {}
-  void removeTask() {}
+  void removeTask(int taskId) {
+    _tasks.remove(taskId);
+    notifyListeners();
+  }
 }
