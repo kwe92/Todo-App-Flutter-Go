2544d15f6ae9a0e879cb2374c38737f06b2fa9f9
Kwe <Kweayon@gmail.com>
2023-08-15 16:30:25 -0500

    update: added descriptive comments

--- lib/features/add_task/ui/add_task_view_model.dart
+++ lib/features/add_task/ui/add_task_view_model.dart
@@ -15,3 +15,16 @@ class AddTaskViewModel extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+// ChangeNotifier
+
+//   - a simple and powerful way to manage state when used with Providers
+//   - can be inherited or mixed in to provide your class with a notification API
+//   - Listeners are added in constant-time O(1) and removed in linear-time O(n)
+
+// notifyListeners
+
+//   - calls all registered listeners (clients)
+//     that are watching an object
+//     should be invoked at the end of a function
+//     that modifies a member variable of a model (ChangeNotifier sub-class)
--- lib/features/add_task/ui/widgets/add_task_text_form_field.dart
+++ lib/features/add_task/ui/widgets/add_task_text_form_field.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/app/themes/theme.dart';
 
-// TODO: Hint TExt
-
-class AddTaskTextForField extends StatelessWidget {
+class AddTaskTextFormField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final int maxLines;
-  const AddTaskTextForField({
+  const AddTaskTextFormField({
     required this.controller,
     required this.hintText,
     this.maxLines = 1,
@@ -28,3 +26,20 @@ class AddTaskTextForField extends StatelessWidget {
         ),
       );
 }
+
+// Dynamic Theming
+
+//   - Wrapping a widget with a Theme Widget then condotionally
+//     changing the argument to the 'data' property
+//     predicated on some condition allows you to dynamically theme that Widget
+//     with Theme.of(context).copyWith() by passing the properties you want to change
+
+// Input Decoration
+
+//   - The borders, labels, icons, and styles for a TextFormField
+//   - passed as an argument to the decoration property 
+ 
+// Multi-line TextFormField
+
+//   - if you want to have a TExtFormField with multiple lines then you can
+//     pass an integer to the maxLines property
