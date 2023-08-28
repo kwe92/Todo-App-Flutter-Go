66acef870d0c6e217ed72bd454d2accf7a4d5fe5
Kwe <Kweayon@gmail.com>
2023-08-28 15:09:54 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/app/themes/theme.dart';
 import 'package:flutter_golang_yt/features/add_task/ui/add_task_view_model.dart';
+import 'package:flutter_golang_yt/features/all_tasks/ui/all_tasks_view_model.dart';
 import 'package:flutter_golang_yt/features/shared/services/get_it.dart';
 import 'package:flutter_golang_yt/features/shared/services/services.dart';
-import 'package:flutter_golang_yt/features/all_tasks/ui/all_tasks_view_model.dart';
 import 'package:provider/provider.dart';
 
 // TODO: maybe implement notification service for old created tasks
@@ -17,7 +17,7 @@ void main() {
         ),
         ChangeNotifierProvider(
           create: (context) => AllTasksViewModel(),
-        )
+        ),
       ],
       child: const MyApp(),
     ),
