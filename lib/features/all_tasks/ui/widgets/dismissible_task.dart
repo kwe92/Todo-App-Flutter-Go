d52e91118365cef726d1f1315890451f11f3960a
Kwe <Kweayon@gmail.com>
2023-08-29 19:05:39 -0500

    update: clear text fields on back

--- lib/features/all_tasks/ui/widgets/dismissible_task.dart
+++ lib/features/all_tasks/ui/widgets/dismissible_task.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/app/colors/app_colors.dart';
 import 'package:flutter_golang_yt/app/router/app_router.gr.dart';
+import 'package:flutter_golang_yt/features/add_task/ui/add_task_view_model.dart';
 import 'package:flutter_golang_yt/features/all_tasks/ui/all_tasks_view_model.dart';
 import 'package:flutter_golang_yt/features/all_tasks/ui/widgets/task_widget.dart';
 import 'package:flutter_golang_yt/features/shared/models/task_model.dart';
@@ -35,13 +36,28 @@ class DismissibleTask extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   CustomButton(
-                    text: 'View',
-                    onPressed: () => appRouter.push(
-                      StandAloneRoute(task: task),
-                    ),
-                  ),
+                      text: 'View',
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                        appRouter.push(
+                          StandAloneRoute(task: task),
+                        );
+                      }),
                   const SizedBox(height: 20.0),
-                  CustomButton(isSecondary: true, text: 'Edit', onPressed: () {}),
+                  CustomButton(
+                    isSecondary: true,
+                    text: 'Edit',
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      context.read<AddTaskViewModel>().loadControllers(
+                            task.taskName,
+                            task.taskDetails,
+                          );
+                      appRouter.push(
+                        EditTaskRoute(task: task),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
