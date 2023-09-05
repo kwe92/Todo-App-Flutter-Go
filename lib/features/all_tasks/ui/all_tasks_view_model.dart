b554476e11f0e12d07d7a0dc49982813ed4f8b28
Kwe <Kweayon@gmail.com>
2023-09-05 18:13:45 -0500

    refactor: completely new implementation with change notifier

--- lib/features/all_tasks/ui/all_tasks_view_model.dart
+++ lib/features/all_tasks/ui/all_tasks_view_model.dart
@@ -3,23 +3,12 @@ import 'package:flutter_golang_yt/features/shared/models/task_model.dart';
 import 'package:flutter_golang_yt/features/shared/services/services.dart';
 
 class AllTasksViewModel extends ChangeNotifier {
-  Map<String, TaskModel>? _tasks;
+  List<TaskModel> _allTasks = taskService.allTasks;
 
-  Map<String, TaskModel>? get tasks => _tasks;
+  List<TaskModel> get allTasks => _allTasks;
 
-  List<TaskModel> get taskList => _tasks!.values.toList();
-
-  void tasksToMap(List<dynamic> allTasks) async {
-    _tasks = {for (var task in allTasks) task['id']: TaskModel.fromJson(task)};
-    await Future.delayed(
-      const Duration(seconds: 2),
-    );
-    notifyListeners();
-  }
-
-  Future<void> removeTask(int taskId) async {
-    _tasks!.remove(taskId);
-    await taskService.deleteTask(taskId);
+  void refresh() {
+    _allTasks = taskService.allTasks;
     notifyListeners();
   }
 }
