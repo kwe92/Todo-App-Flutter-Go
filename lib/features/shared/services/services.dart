b542e6cccfb693de61376ed221ba92c329c03668
Kwe <Kweayon@gmail.com>
2023-09-06 17:21:19 -0500

    update: added taskService computed variable

--- lib/features/shared/services/services.dart
+++ lib/features/shared/services/services.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_golang_yt/app/router/app_router.dart';
 import 'package:flutter_golang_yt/features/shared/services/get_it.dart';
 import 'package:flutter_golang_yt/features/shared/services/task_service.dart';
+import 'package:flutter_golang_yt/features/shared/services/toast_service.dart';
 import 'package:http/http.dart' as http;
 
 AppRouter get appRouter => locator.get<AppRouter>();
@@ -8,6 +9,9 @@ AppRouter get appRouter => locator.get<AppRouter>();
 TaskService get taskService => locator.get<TaskService>();
 
 http.Client get client => locator.get<http.Client>();
+
+ToastService get toastService => locator.get<ToastService>();
+
 
 
 // Services Module
