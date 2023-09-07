ae3b74f45d798589894a6f6d5823c5123e1b01ab
Kwe <Kweayon@gmail.com>
2023-09-07 07:42:21 -0500

    refact: refactored implementations to use internal state

--- lib/features/shared/services/task_service.dart
+++ lib/features/shared/services/task_service.dart
@@ -9,8 +9,7 @@ import 'api_service.dart';
 
 // TODO: handle if the response is an array or error map | check the response type
 
-/// TaskService is a stub for sending requests to the task API endpoints.
-
+/// TaskService sends requests to the task API endpoints.
 class TaskService extends ApiService {
   @override
   String get host => "http://127.0.0.1:8082";
@@ -22,10 +21,14 @@ class TaskService extends ApiService {
 
   List<TaskModel> _allTasks = [];
 
+  List<TaskModel> _singleTask = [];
+
   List<TaskModel> get allTasks => _allTasks;
 
+  List<TaskModel> get singleTask => _singleTask;
+
   /// getAllTasks requests all tasks from the server.
-  Future<dynamic> getAllTasks() async {
+  Future<List<TaskModel>> getAllTasks() async {
     final http.Response response = await get(EndPoint.gettasks.path);
 
     final List<dynamic> tasks = jsonDecode(response.body);
@@ -34,43 +37,59 @@ class TaskService extends ApiService {
 
     _allTasks = taskList;
 
-    debugPrint("\n\nFrom getAllTasks: $taskList");
+    debugPrint("\nFrom getAllTasks: $taskList");
 
     return taskList;
   }
 
   /// getTaskById request a task by id from the server.
-
-  Future<dynamic> getTaskById(int id) async {
+  Future<void> getTaskById(int id) async {
     final http.Response response = await get(
       EndPoint.gettask.path + id.toString(),
     );
-    return jsonDecode(response.body);
+
+    final Map<String, dynamic> task = jsonDecode(response.body);
+
+    final taskList = <TaskModel>[TaskModel.fromJson(task)];
+
+    _singleTask = taskList;
+
+    debugPrint("\nFrom getTaskById: $taskList");
   }
 
   /// createTask requests to add a task to the server and receives a response of all tasks.
-
-  Future<dynamic> createTask(Map<String, dynamic> task) async {
+  Future<void> createTask(Map<String, dynamic> task) async {
     final http.Response response = await post(
       EndPoint.create.path,
       body: jsonEncode(task),
     );
-    return jsonDecode(response.body);
+
+    final List<dynamic> tasks = jsonDecode(response.body);
+
+    final taskList = <TaskModel>[for (Map<String, dynamic> task in tasks) TaskModel.fromJson(task)];
+
+    _allTasks = taskList;
+
+    debugPrint("\nFrom createTask: $taskList");
   }
 
   /// updateTask updates the given task if it exists.
-
-  Future<dynamic> updateTask(Map<String, dynamic> task) async {
+  Future<void> updateTask(Map<String, dynamic> task) async {
     final response = await put(
       EndPoint.update.path + task['id'],
       body: jsonEncode(task),
     );
 
-    return jsonDecode(response.body);
+    final List<dynamic> tasks = jsonDecode(response.body);
+
+    final taskList = <TaskModel>[for (Map<String, dynamic> task in tasks) TaskModel.fromJson(task)];
+
+    _allTasks = taskList;
+
+    debugPrint("\nFrom updateTask: $taskList");
   }
 
   /// deleteTask removes a task with the specified id.
-
   Future<void> deleteTask(int taskId) async {
     final response = await delete(
       EndPoint.delete.path + taskId.toString(),
@@ -87,7 +106,6 @@ class TaskService extends ApiService {
 }
 
 /// EndPoint is an enumerated list of all task endpoints.
-
 enum EndPoint {
   gettasks("/gettasks"),
 
