e943f9faf9f874fb12687d2f2bcc1e7be3674524
Kwe <Kweayon@gmail.com>
2023-09-17 19:36:06 -0500

    update: added initial render fade in

--- lib/features/all_tasks/ui/widgets/top_section.dart
+++ lib/features/all_tasks/ui/widgets/top_section.dart
@@ -1,3 +1,4 @@
+import 'package:entry/entry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_golang_yt/app/colors/app_colors.dart';
 import 'package:flutter_golang_yt/features/shared/services/services.dart';
@@ -8,23 +9,26 @@ class TopSecton extends StatelessWidget {
   const TopSecton({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-        width: double.maxFinite,
-        height: ScreenSize.getHeight(context) / 3.5,
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.only(
-          top: 18,
-          left: 12,
-        ),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/images/coffee-table5.avif'),
+  Widget build(BuildContext context) => Entry.opacity(
+        duration: const Duration(seconds: 1, milliseconds: 500),
+        child: Container(
+          width: double.maxFinite,
+          height: ScreenSize.getHeight(context) / 3.5,
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(
+            top: 18,
+            left: 12,
           ),
-        ),
-        child: BackArrowIcon(
-          color: AppColors.green0,
-          onTap: () => appRouter.pop(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/coffee-table5.avif'),
+            ),
+          ),
+          child: BackArrowIcon(
+            color: AppColors.green0,
+            onTap: () => appRouter.pop(),
+          ),
         ),
       );
 }
