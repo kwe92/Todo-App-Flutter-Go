f80d8de28ddb45d3fa5214ffae71fb764b18e9c9
Kwe <Kweayon@gmail.com>
2023-09-26 10:12:27 -0500

    turned off requrest to fonts.google.com for the google fonts pacjage

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,13 @@ import 'package:flutter_golang_yt/features/edit_task/ui/edit_task_view_model.dar
 import 'package:flutter_golang_yt/features/shared/services/get_it.dart';
 import 'package:flutter_golang_yt/features/shared/services/services.dart';
 import 'package:flutter_golang_yt/features/standalone/ui/standalone_view_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 // TODO: maybe implement notification service for old created tasks
 void main() {
   configureDependencies();
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(
     MultiProvider(
       providers: [
